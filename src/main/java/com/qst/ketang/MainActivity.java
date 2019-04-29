8ac998892521f8a09377bbf5ec9827d30afa4da3
PYJisDad <645541585@qq.com>
2019-04-29 12:00:57 +0800

    第一次提交 整个项目 可以看到修改了哪些文件

--- src/main/java/com/qst/ketang/MainActivity.java
+++ src/main/java/com/qst/ketang/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        //随随便便改点什么
+
     }
 }
