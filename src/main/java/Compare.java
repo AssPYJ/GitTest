e65a2a9211ec3ed90aba9b60ddde00a8cb24a1be
PYJisDad <645541585@qq.com>
2019-04-29 11:59:24 +0800

    第一次提交 整个项目

---
+++ src/main/java/Compare.java
@@ -0,0 +1,14 @@
+public class Compare {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+    }
+    public  static void CompareNum(int a,int b){
+        if (a > b){
+            System.out.println (a+"大");
+        }else {
+            System.out.println (b+"大");
+
+        }
+    }
+}
